7cc049793d5c96a5f9ceebb089b2e08e2b2448a2
cervantes21 <admon.owlsoul@gmail.com>
2023-05-07 05:48:26 -0600

    Arrays

---
+++ Curso_Estructuras_de_datos/Arrays/array.py
@@ -0,0 +1,23 @@
+class Array:
+    def __init__(self, capacity, fill_value=None):
+        self.items = list()
+        for i in range(capacity):
+            self.items.append(fill_value)
+
+    def __len__(self):
+        return len(self.items)
+
+    
+    def __str__(self):
+        return str(self.items)
+
+    
+    def __iter__(self):
+        return iter(self.items)
+
+    def __getitem__(self,index):
+        return self.items[index]
+
+    def __setitem__(self,index,new_item):
+        self.items[index] = new_item
+
